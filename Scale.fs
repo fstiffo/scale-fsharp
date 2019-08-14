b42c1216aa3444d227b5930933b94fb44f8429c4
Francesco Stiffoni <fstiffo@sgajo.com>
2019-08-14 14:51:15 +0200

    Code reformatted with Fantomas

--- Scale.fs
+++ Scale.fs
@@ -1,4 +1,3 @@
-﻿
 module Scale
 
 open System
@@ -6,12 +5,15 @@ open System.IO
 open System.Runtime.Serialization.Formatters.Soap
 open Itenso.TimePeriod
 
+[<Serializable>]
+type Condomino =
+    | Michela
+    | Gerardo
+    | Elena
+    | Giulia
 
 [<Serializable>]
-type Condomino = Michela | Gerardo | Elena | Giulia
-
-[<Serializable>]
-type Operazione = 
+type Operazione =
     | VersamentoQuote of Condomino * int
     | PagamentoScale
     | AltraSpesa of string * int
@@ -20,24 +22,26 @@ type Operazione =
     | Restituzione of int
 
 [<Serializable>]
-type Data = Data of int * int  * int
+type Data = Data of int * int * int
 
 [<Serializable>]
-type Movimento = (Data * Operazione)
+type Movimento = Data * Operazione
 
 [<Serializable>]
-type Param = { costoScale: int; numPulizieMese: int; quotaMensile: int }
+type Param =
+    { costoScale : int
+      numPulizieMese : int
+      quotaMensile : int }
 
 [<Serializable>]
-type Attuale = (Data * Param)
+type Attuale = Data * Param
 
 [<Serializable>]
-type Stato = {
-    tempoZero: Data;
-    attuale: Attuale;
-    condomini: Condomino list;
-    movimenti: Movimento list    
-}
+type Stato =
+    { tempoZero : Data
+      attuale : Attuale
+      condomini : Condomino list
+      movimenti : Movimento list }
 
 let createStato fileName : Stato =
     if (File.Exists(fileName)) then
@@ -45,74 +49,96 @@ let createStato fileName : Stato =
         let fsStato = new FileStream(fileName, FileMode.Open)
         sfStato.Deserialize(fsStato) :?> Stato
     else
-        let t_0 = Data (2019,7,1)
-        let s : Stato = {
-               tempoZero = t_0;
-               attuale = (t_0, { costoScale = 20; numPulizieMese=2; quotaMensile=12 });
-               condomini = [Michela; Gerardo; Elena; Giulia];
-               movimenti = [
-                 (t_0, (AltroVersamento ("Appianamento", 333)));
-                 (t_0, VersamentoQuote (Michela, 74));
-                 (t_0, VersamentoQuote (Gerardo, 78));
-                 (t_0, VersamentoQuote (Elena, 48));
-                 (Data (2019, 7, 22), Prestito 500);
-                 (Data (2019, 7, 11), PagamentoScale)
-               ]
-             }
-        let sfStato = new SoapFormatter()        
+        let t_0 = Data(2019, 7, 1)
+
+        let s : Stato =
+            { tempoZero = t_0
+              attuale =
+                  (t_0,
+                   { costoScale = 20
+                     numPulizieMese = 2
+                     quotaMensile = 12 })
+              condomini = [ Michela; Gerardo; Elena; Giulia ]
+              movimenti =
+                  [ (t_0, (AltroVersamento("Appianamento", 333)))
+                    (t_0, VersamentoQuote(Michela, 74))
+                    (t_0, VersamentoQuote(Gerardo, 78))
+                    (t_0, VersamentoQuote(Elena, 48))
+                    (Data(2019, 7, 22), Prestito 500)
+                    (Data(2019, 7, 11), PagamentoScale)
+                    (Data(2019, 8, 14), Restituzione 200)
+                    (Data(2019, 8, 14), PagamentoScale) ] }
+
+        let sfStato = new SoapFormatter()
         let fsStato = new FileStream(fileName, FileMode.Create)
         sfStato.Serialize(fsStato, s)
         s
 
-let private contabile (s: Stato) ((_,op): Movimento) : int = 
+let private contabile (s : Stato) ((_, op) : Movimento) : int =
     match op with
-    | VersamentoQuote (_, i) -> i
+    | VersamentoQuote(_, i) -> i
     | PagamentoScale -> -(snd s.attuale).costoScale
-    | AltraSpesa (_, i) -> - i
-    | AltroVersamento (_, i) -> i
-    | Prestito i -> - i
+    | AltraSpesa(_, i) -> -i
+    | AltroVersamento(_, i) -> i
+    | Prestito i -> -i
     | Restituzione i -> i
 
-let cassa (s: Stato) : int =
-    s.movimenti |> List.map (contabile s) |> List.sum
+let cassa (s : Stato) : int =
+    s.movimenti
+    |> List.map (contabile s)
+    |> List.sum
 
-let private altroContabile (s: Stato) ((_,op): Movimento) : int =
+let private altroContabile (s : Stato) ((_, op) : Movimento) : int =
     match op with
-    | AltraSpesa (_, i) -> -i
-    | AltroVersamento (_, i) -> i
+    | AltraSpesa(_, i) -> -i
+    | AltroVersamento(_, i) -> i
     | _ -> 0
 
-let private pagamentoScale (s: Stato) ((_,op): Movimento) : int =
+let private pagamentoScale (s : Stato) ((_, op) : Movimento) : int =
     match op with
     | PagamentoScale -> (snd s.attuale).costoScale
     | _ -> 0
 
-let dataToDateTime(Data (y,m,d)) =
-    new DateTime(y,m,d)
+let dataToDateTime (Data(y, m, d)) = new DateTime(y, m, d)
 
-let tesoretto (s: Stato)  : int =
-    let altro = s.movimenti |> List.map (altroContabile s) |> List.sum
-    let pagamenti = s.movimenti |> List.map (pagamentoScale s) |> List.sum
+let tesoretto (s : Stato) : int =
+    let altro =
+        s.movimenti
+        |> List.map (altroContabile s)
+        |> List.sum
+
+    let pagamenti =
+        s.movimenti
+        |> List.map (pagamentoScale s)
+        |> List.sum
+
     let mesi = (new DateDiff(dataToDateTime s.tempoZero, DateTime.Today)).Months
     let num_condomini = List.length s.condomini
     mesi * num_condomini * (snd s.attuale).quotaMensile + altro - pagamenti
 
-let private versamentoQuote (s: Stato) (con: Condomino) ((_,op): Movimento)  : int =
+let private versamentoQuote (s : Stato) (con : Condomino) ((_, op) : Movimento) : int =
     match op with
-    | VersamentoQuote (c, i) when c = con -> i
+    | VersamentoQuote(c, i) when c = con -> i
     | _ -> 0
- 
-let quote (s: Stato) (c: Condomino) : int =
-    let versamentiQuote =  s.movimenti |> List.map (versamentoQuote s c) |> List.sum
+
+let quote (s : Stato) (c : Condomino) : int =
+    let versamentiQuote =
+        s.movimenti
+        |> List.map (versamentoQuote s c)
+        |> List.sum
+
     let quoteVersate = versamentiQuote / (snd s.attuale).quotaMensile
     let mesi = (new DateDiff(dataToDateTime s.tempoZero, DateTime.Today)).Months
     quoteVersate - mesi
 
-let prestitiContabile (s: Stato) ((_,op): Movimento) : int =
+let prestitiContabile (s : Stato) ((_, op) : Movimento) : int =
     match op with
     | Prestito i -> -i
     | Restituzione i -> i
     | _ -> 0
 
-let prestito (s: Stato) : int =
-     s.movimenti |> List.map  (prestitiContabile s) |> List.sum |> ( ~- )
+let prestito (s : Stato) : int =
+    s.movimenti
+    |> List.map (prestitiContabile s)
+    |> List.sum
+    |> (~-)
--- UI.fs
+++ UI.fs
@@ -4,33 +4,37 @@ open System
 open Terminal.Gui
 open NStack
 
-let ustr (x:string) = ustring.Make(x)
-
+let ustr (x : string) = ustring.Make(x)
 let stop = Action Application.RequestStop
 
 let newFile() =
-    let ok = Button (ustr "Ok", true, Clicked=stop)
-    let cancel = Button (ustr "Cancel", Clicked=stop)
-    let d = Dialog (ustr ("New File"), 50, 20, ok, cancel)
-    Application.Run (d)
+    let ok = Button(ustr "Ok", true, Clicked = stop)
+    let cancel = Button(ustr "Cancel", Clicked = stop)
+    let d = Dialog(ustr ("New File"), 50, 20, ok, cancel)
+    Application.Run(d)
 
 let quit() =
-    if MessageBox.Query (50, 7, "Quit demo", "Are you sure you want to quit the demo?", "Yes", "No") = 0 then
-       Application.Top.Running <- false
+    if MessageBox.Query
+           (50, 7, "Quit demo", "Are you sure you want to quit the demo?", "Yes",
+            "No") = 0 then Application.Top.Running <- false
 
 let buildMenu() =
-    MenuBar ([|
-        MenuBarItem (ustr ("_File"), 
-            [| MenuItem(ustr("_New"), "Creates a new file", System.Action newFile);
-               MenuItem(ustr("_Quit"), null, System.Action quit)
-             |])|])
+    MenuBar
+        ([| MenuBarItem
+                (ustr ("_File"),
+                 [| MenuItem
+                        (ustr ("_New"), "Creates a new file",
+                         System.Action newFile)
+                    MenuItem(ustr ("_Quit"), null, System.Action quit) |]) |])
 
 let startApp =
-    Application.Init ()
+    Application.Init()
     Colors.Base.Focus <- Attribute.Make(Color.BrightGreen, Color.Brown)
-
     let top = Application.Top
-    let win = Window (ustr "Hello", X=Pos.op_Implicit(0), Y=Pos.op_Implicit(1), Width=Dim.Fill(), Height=Dim.Fill())
-    top.Add (buildMenu())
-    top.Add (win)
-    Application.Run ()
+    let win =
+        Window
+            (ustr "Hello", X = Pos.op_Implicit (0), Y = Pos.op_Implicit (1),
+             Width = Dim.Fill(), Height = Dim.Fill())
+    top.Add(buildMenu())
+    top.Add(win)
+    Application.Run()
