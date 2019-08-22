83b03774415a025e9ad99a330afaa254de4165a3
Francesco Stiffoni <fstiffo@sgajo.com>
2019-08-22 09:31:05 +0200

    Remove debug code

--- Program.fs
+++ Program.fs
@@ -6,11 +6,4 @@ open UI
 let main argv =
     createStato()
     startApp()
-    printfn "%A" Scale.stato
-    printfn "%d" (cassa Scale.stato)
-    printfn "%d" (tesoretto Scale.stato)
-    printfn "%d %d %d %d" (quote Scale.stato Michela)
-        (quote Scale.stato Gerardo) (quote Scale.stato Elena)
-        (quote Scale.stato Giulia)
-    printfn "%d" (prestito Scale.stato)
     0 // return an integer exit code
